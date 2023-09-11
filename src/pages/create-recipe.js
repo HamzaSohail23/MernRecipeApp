57625d7c8daf37b0819bd90a7bf23a1595afa86b
hamza <hamza.sohail.hcs@gmail.com>
2023-09-10 19:19:36 -0500

    fixed creatw-recipe

--- src/pages/create-recipe.js
+++ src/pages/create-recipe.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useGetUserID } from "../hooks/useGetUserID";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import { useGetUserID } from "../hooks/useGetUserID";
 
 export const CreateRecipe = () => {
   const userID = useGetUserID();
-  const [cookies, _] = useCookies(["access_token"]);
+  const [cookies] = useCookies(["access_token"]);
   const [recipe, setRecipe] = useState({
     name: "",
     description: "",
@@ -38,8 +38,15 @@ export const CreateRecipe = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    
+    // Check if userID is available
+    if (!userID) {
+      console.error("User ID is not available.");
+      return;
+    }
+
     try {
-      await axios.post(
+      const response = await axios.post(
         "https://mern-recipe-app-f68043e5c805.herokuapp.com/recipes",
         { ...recipe },
         {
@@ -47,8 +54,12 @@ export const CreateRecipe = () => {
         }
       );
 
-      alert("Recipe Created");
-      navigate("/");
+      if (response.status === 201) {
+        alert("Recipe Created");
+        navigate("/");
+      } else {
+        console.error("Failed to create recipe.");
+      }
     } catch (error) {
       console.error(error);
     }
@@ -65,6 +76,7 @@ export const CreateRecipe = () => {
           name="name"
           value={recipe.name}
           onChange={handleChange}
+          required
         />
         <label htmlFor="description">Description</label>
         <textarea
@@ -72,6 +84,7 @@ export const CreateRecipe = () => {
           name="description"
           value={recipe.description}
           onChange={handleChange}
+          required
         ></textarea>
         <label htmlFor="ingredients">Ingredients</label>
         {recipe.ingredients.map((ingredient, index) => (
@@ -81,6 +94,7 @@ export const CreateRecipe = () => {
             name="ingredients"
             value={ingredient}
             onChange={(event) => handleIngredientChange(event, index)}
+            required
           />
         ))}
         <button type="button" onClick={handleAddIngredient}>
@@ -92,6 +106,7 @@ export const CreateRecipe = () => {
           name="instructions"
           value={recipe.instructions}
           onChange={handleChange}
+          required
         ></textarea>
         <label htmlFor="imageUrl">Image URL</label>
         <input
